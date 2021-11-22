a6a3e7a009e04b637d5c55de006f685f7f8a539e
Giovanni Coloma Heidel <giovanni.coloma-heidel@conweaver.onmicrosoft.com>
2021-11-22 20:19:50 +0100

    Fixed bishop movement allowence, added bishop test

--- Game/Pieces/BishopPiece.cs
+++ Game/Pieces/BishopPiece.cs
@@ -20,7 +20,32 @@ namespace GameLogic.Pieces
 
         protected override bool IsTargetPositionAllowed(Field field, Position targetPosition)
         {
-            return field.GetPieceAt(targetPosition)?.Color != Color && _basicMovements.IsTargetPositionAllowed(Position, targetPosition);
+            // check if target position is valid move
+            if (!_basicMovements.IsTargetPositionAllowed(Position, targetPosition))
+            {
+                return false;
+            }
+
+            // check if there is a piece in the way to target position
+            var dirX = targetPosition.X > Position.X ? 1 : -1;
+            var dirY = targetPosition.Y > Position.Y ? 1 : -1;
+
+            // avoid check, if next cell would be target position
+            if (Position.X + dirX != targetPosition.X)
+            {
+                var y = Position.Y;
+                for (var x = Position.X + dirX; x != targetPosition.X; x += dirX)
+                {
+                    y += dirY;
+                    if (field.GetPieceAt(new Position(x, y)) != null)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+
+            return field.GetPieceAt(targetPosition)?.Color != Color;
         }
     }
 }
---
+++ GameLogic.Test/PieceTest/BishopTest.cs
@@ -0,0 +1,163 @@
+﻿using FluentAssertions;
+using GameLogic.FieldParser;
+using GameLogic.Pieces;
+using Xunit;
+
+namespace GameLogic.Test.PieceTest
+{
+    public class BishopTest
+    {
+        [Fact]
+        public void AllowedMovesWithNoObstacles()
+        {
+            const string fieldLayout = @"P---K---
+                                         ----P---
+                                         --------
+                                         ----b---
+                                         ----k---
+                                         --------
+                                         ----p---
+                                         -------p";
+
+            var expectedMoves = new[]
+            {
+                new Position(3,3),
+                new Position(2,2),
+                new Position(1,1),
+                new Position(0,0),
+                new Position(5,3),
+                new Position(6,2),
+                new Position(7,1),
+                new Position(5,5),
+                new Position(6,6),
+                new Position(7,7),
+                new Position(3,5),
+                new Position(2,6),
+                new Position(1,7)
+            };
+
+            var simpleStringLayoutParser = new SingleBoardSimpleStringLayoutParser();
+            var field = simpleStringLayoutParser.CreateField(fieldLayout);
+            var bishop = field.GetPieceAt(new Position(4, 4));
+
+            Assert.IsType<BishopPiece>(bishop);
+            var actualMoves = bishop?.GetAllowedMoves(field);
+            actualMoves.Should().HaveSameCount(expectedMoves).And.Contain(expectedMoves);
+        }
+
+        [Fact]
+        public void AllowedMovesWithObstacles()
+        {
+            const string fieldLayout = @"P---K---
+                                         ----P---
+                                         -----P--
+                                         ----b---
+                                         ---k----
+                                         ---p----
+                                         ----p---
+                                         -------p";
+
+            var expectedMoves = new[]
+            {
+                new Position(5,3),
+                new Position(6,2),
+                new Position(7,1),
+                new Position(5,5),
+                new Position(3,5),
+                new Position(2,6),
+                new Position(1,7)
+            };
+
+            var simpleStringLayoutParser = new SingleBoardSimpleStringLayoutParser();
+            var field = simpleStringLayoutParser.CreateField(fieldLayout);
+            var bishop = field.GetPieceAt(new Position(4, 4));
+
+            Assert.IsType<BishopPiece>(bishop);
+            var actualMoves = bishop?.GetAllowedMoves(field);
+            actualMoves.Should().HaveSameCount(expectedMoves).And.Contain(expectedMoves);
+        }
+
+        [Fact]
+        public void WithNoObstacles_MoveIsAllowed()
+        {
+            const string fieldLayout = @"P---K---
+                                         ----P---
+                                         --------
+                                         ----b---
+                                         ----k---
+                                         --------
+                                         ----p---
+                                         -------p";
+
+            var simpleStringLayoutParser = new SingleBoardSimpleStringLayoutParser();
+            var field = simpleStringLayoutParser.CreateField(fieldLayout);
+            var bishop = field.GetPieceAt(new Position(4, 4));
+
+            Assert.IsType<BishopPiece>(bishop);
+            bishop?.IsMoveAllowed(field, new Position(3, 3)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(2, 2)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(1, 1)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(0, 0)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(5, 3)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(6, 2)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(7, 1)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(5, 5)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(6, 6)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(7, 7)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(3, 5)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(2, 6)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(1, 7)).Should().BeTrue();
+        }
+
+        [Fact]
+        public void WithObstacles_MoveIsAllowed()
+        {
+            const string fieldLayout = @"P---K---
+                                         ----P---
+                                         -----P--
+                                         ----b---
+                                         ---k----
+                                         ---p----
+                                         ----p---
+                                         -------p";
+
+            var simpleStringLayoutParser = new SingleBoardSimpleStringLayoutParser();
+            var field = simpleStringLayoutParser.CreateField(fieldLayout);
+            var bishop = field.GetPieceAt(new Position(4, 4));
+
+            Assert.IsType<BishopPiece>(bishop);
+            bishop?.IsMoveAllowed(field, new Position(5, 3)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(6, 2)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(7, 1)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(5, 5)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(3, 5)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(2, 6)).Should().BeTrue();
+            bishop?.IsMoveAllowed(field, new Position(1, 7)).Should().BeTrue();
+        }
+
+        [Fact]
+        public void WithObstacles_MoveIsNotAllowed()
+        {
+            const string fieldLayout = @"P---K---
+                                         ----P---
+                                         -----P--
+                                         ----b---
+                                         ---k----
+                                         ---p----
+                                         ----p---
+                                         -------p";
+
+            var simpleStringLayoutParser = new SingleBoardSimpleStringLayoutParser();
+            var field = simpleStringLayoutParser.CreateField(fieldLayout);
+            var bishop = field.GetPieceAt(new Position(4, 4));
+
+            Assert.IsType<BishopPiece>(bishop);
+            bishop?.IsMoveAllowed(field, new Position(3, 3)).Should().BeFalse();
+            bishop?.IsMoveAllowed(field, new Position(2, 2)).Should().BeFalse();
+            bishop?.IsMoveAllowed(field, new Position(1, 1)).Should().BeFalse();
+            bishop?.IsMoveAllowed(field, new Position(0, 0)).Should().BeFalse();
+            bishop?.IsMoveAllowed(field, new Position(6, 6)).Should().BeFalse();
+            bishop?.IsMoveAllowed(field, new Position(7, 7)).Should().BeFalse();
+        }
+    }
+}
