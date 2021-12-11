8370873c193517931db268ad5b53fe3eb1ebb71f
Giovanni Coloma Heidel <giovanni.coloma-heidel@conweaver.onmicrosoft.com>
2021-12-11 21:25:42 +0100

    removed two board parser

--- Game/Pieces.cs
+++ Game/Pieces.cs
@@ -17,20 +17,20 @@
 
     public class Pieces
     {
-        public static Piece Queen { get; } = new Piece("q");
-        public static Piece Rook { get; } = new Piece("r");
-        public static Piece Knight { get; } = new Piece("n");
-        public static Piece Bishop { get; } = new Piece("b");
-        public static Piece Pawn { get; } = new Piece("p");
-        public static Piece King { get; } = new Piece("k");
+        public static Piece Queen { get; } = new Piece("Q");
+        public static Piece Rook { get; } = new Piece("R");
+        public static Piece Knight { get; } = new Piece("N");
+        public static Piece Bishop { get; } = new Piece("B");
+        public static Piece Pawn { get; } = new Piece("P");
+        public static Piece King { get; } = new Piece("N");
     }
 
     public class TradablePieces
     {
-        public static Piece Queen { get; } = new Piece("q");
-        public static Piece Rook { get; } = new Piece("r");
-        public static Piece Knight { get; } = new Piece("n");
-        public static Piece Bishop { get; } = new Piece("b");
+        public static Piece Queen { get; } = Pieces.Queen;
+        public static Piece Rook { get; } = Pieces.Rook;
+        public static Piece Knight { get; } = Pieces.Knight;
+        public static Piece Bishop { get; } = Pieces.Bishop;
     }
 
     public class ColoredPieces
