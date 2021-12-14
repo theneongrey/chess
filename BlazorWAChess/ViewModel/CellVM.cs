403894fe89c0b12ccbd80788a2d206cb23956b7b
Giovanni Coloma Heidel <giovanni.coloma-heidel@conweaver.onmicrosoft.com>
2021-12-14 11:18:43 +0100

    initial commit: board is displayed

---
+++ BlazorWAChess/ViewModel/CellVM.cs
@@ -0,0 +1,19 @@
+﻿using GameLogic;
+
+namespace BlazorWAChess.ViewModel
+{
+    public class CellVM
+    {
+        private Piece? _piece;
+
+        internal void Update(Piece? piece)
+        {
+            _piece = piece;
+        }
+
+        public override string ToString()
+        {
+            return _piece?.Identifier ?? string.Empty;
+        }
+    }
+}
---
+++ BlazorWAChess/ViewModel/GameVM.cs
@@ -0,0 +1,44 @@
+﻿using GameLogic;
+
+namespace BlazorWAChess.ViewModel
+{
+    public class GameVM
+    {
+        public CellVM[][] Cells { get; private set; }
+
+        public GameVM()
+        {
+            Cells = new CellVM[8][];
+            CreateCells();
+        }
+
+        private void CreateCells()
+        {
+            for (var y = 0; y < Cells.Length; y++)
+            {
+                Cells[y] = new CellVM[8];
+                for (var x = 0; x < Cells[y].Length; x++)
+                {
+                    Cells[y][x] = new CellVM();
+                }
+            }
+        }
+
+        public void Update(Game game)
+        {
+            UpdateCells(game);
+        }
+
+        private void UpdateCells(Game game)
+        {
+            var currentCells = game.GetBoard();
+            for (var y = 0; y < currentCells.Length; y++)
+            {
+                for (var x = 0; x < currentCells[y].Length; x++)
+                {
+                    Cells[y][x].Update(currentCells[y][x]);
+                }
+            }
+        }
+    }
+}
