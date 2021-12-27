4f95252d7c88f10e7ca0cad79aaad2622044b6d7
Giovanni Coloma Heidel <giovanni.coloma-heidel@conweaver.onmicrosoft.com>
2021-12-27 21:12:07 +0100

    refactored razor ui model

---
+++ BlazorWAChess/ViewModel/MovesVM.cs
@@ -0,0 +1,21 @@
+﻿using GameLogic;
+
+namespace BlazorWAChess.ViewModel
+{
+    public class MovesVM
+    {
+        private Game _game;
+        public IEnumerable<string> Moves { get; private set; }
+
+        public MovesVM(Game game)
+        {
+            _game = game;
+            Moves = Array.Empty<string>();
+        }
+
+        public void Update()
+        {
+            Moves = _game.ToFullAlgebraicNotationArray();
+        }
+    }
+}
