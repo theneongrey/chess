617c145ea1fa313093a66ae4aa7ad168d7a07aa6
Giovanni Coloma Heidel <giovanni.coloma-heidel@conweaver.onmicrosoft.com>
2022-02-27 14:14:56 +0100

    cleaned up fileStoreSetvice Tests

--- MinimalChessApi.Test/Unit/FileStoreServiceTest.cs
+++ MinimalChessApi.Test/Unit/FileStoreServiceTest.cs
@@ -13,7 +13,14 @@ namespace MinimalChessApi.Test.Unit
     public class FileStoreServiceTest
     {
         private const string TargetPath = "_gamesTest";
+        private IFileSystem _fileSystemMock;
+        private FileStoreService _sut;
 
+        public FileStoreServiceTest()
+        {
+            _fileSystemMock = Substitute.For<IFileSystem>();
+            _sut = new FileStoreService(_fileSystemMock, TargetPath);
+        }
 
         #region save game
         [Fact]
@@ -21,15 +28,13 @@ namespace MinimalChessApi.Test.Unit
         {
             // Arrange
             string? actualFileContent = null;
-            var fileSystemMock = Substitute.For<IFileSystem>();
-            fileSystemMock.Directory.Exists(TargetPath).Returns(true);
+            _fileSystemMock.Directory.Exists(TargetPath).Returns(true);
 
-            await fileSystemMock.File.WriteAllTextAsync(Arg.Any<string>(), Arg.Do<string>(c => actualFileContent = c));
+            await _fileSystemMock.File.WriteAllTextAsync(Arg.Any<string>(), Arg.Do<string>(c => actualFileContent = c));
 
-            var sut = new FileStoreService(fileSystemMock, TargetPath);
 
             // Act
-            var result = await sut.SaveGameAsync(Guid.NewGuid(), string.Empty);
+            var result = await _sut.SaveGameAsync(Guid.NewGuid(), string.Empty);
 
             // Assert
             result.Should().BeTrue();
@@ -44,15 +49,12 @@ namespace MinimalChessApi.Test.Unit
 2.Ng1-h3 Bf8-e7
 3.a2-a4 Be7-h4+";
             string? actualFileContent = null;
-            var fileSystemMock = Substitute.For<IFileSystem>();
-            fileSystemMock.Directory.Exists(TargetPath).Returns(true);
+            _fileSystemMock.Directory.Exists(TargetPath).Returns(true);
 
-            await fileSystemMock.File.WriteAllTextAsync(Arg.Any<string>(), Arg.Do<string>(c => actualFileContent = c));
-
-            var sut = new FileStoreService(fileSystemMock, TargetPath);
+            await _fileSystemMock.File.WriteAllTextAsync(Arg.Any<string>(), Arg.Do<string>(c => actualFileContent = c));
 
             // Act
-            var result = await sut.SaveGameAsync(Guid.NewGuid(), fileContent);
+            var result = await _sut.SaveGameAsync(Guid.NewGuid(), fileContent);
 
             // Assert
             result.Should().BeTrue();
@@ -63,15 +65,11 @@ namespace MinimalChessApi.Test.Unit
         public async Task SaveGame_OnWriteException_ReturnsFalse()
         {
             // Arrange
-            var fileSystemMock = Substitute.For<IFileSystem>();
-            fileSystemMock.Directory.Exists(TargetPath).Returns(true);
-
-            fileSystemMock.File.WriteAllTextAsync(Arg.Any<string>(), Arg.Any<string>()).Returns(b => throw new Exception());
-
-            var sut = new FileStoreService(fileSystemMock, TargetPath);
+            _fileSystemMock.Directory.Exists(TargetPath).Returns(true);
+            _fileSystemMock.File.WriteAllTextAsync(Arg.Any<string>(), Arg.Any<string>()).Returns(b => throw new Exception());
 
             // Act
-            var result = await sut.SaveGameAsync(Guid.NewGuid(), string.Empty);
+            var result = await _sut.SaveGameAsync(Guid.NewGuid(), string.Empty);
 
             // Assert
             result.Should().BeFalse();
@@ -88,15 +86,12 @@ namespace MinimalChessApi.Test.Unit
             var gameId = Guid.NewGuid();
             var filename = Path.Combine(TargetPath, $"{gameId}.game"); ;
 
-            var fileSystemMock = Substitute.For<IFileSystem>();
-            fileSystemMock.Directory.Exists(TargetPath).Returns(true);
-            fileSystemMock.File.Exists(filename).Returns(true);
-            fileSystemMock.File.ReadAllTextAsync(filename).Returns(fileContent);
-
-            var sut = new FileStoreService(fileSystemMock, TargetPath);
+            _fileSystemMock.Directory.Exists(TargetPath).Returns(true);
+            _fileSystemMock.File.Exists(filename).Returns(true);
+            _fileSystemMock.File.ReadAllTextAsync(filename).Returns(fileContent);
 
             // Act
-            var result = await sut.LoadGameAsync(new Guid());
+            var result = await _sut.LoadGameAsync(new Guid());
 
             // Assert
             result.Should().Be(fileContent);
@@ -109,14 +104,11 @@ namespace MinimalChessApi.Test.Unit
             var gameId = Guid.NewGuid();
             var filename = Path.Combine(TargetPath, $"{gameId}.game"); ;
 
-            var fileSystemMock = Substitute.For<IFileSystem>();
-            fileSystemMock.Directory.Exists(TargetPath).Returns(true);
-            fileSystemMock.File.Exists(filename).Returns(false);
-
-            var sut = new FileStoreService(fileSystemMock, TargetPath);
+            _fileSystemMock.Directory.Exists(TargetPath).Returns(true);
+            _fileSystemMock.File.Exists(filename).Returns(false);
 
             // Act
-            var result = await sut.LoadGameAsync(new Guid());
+            var result = await _sut.LoadGameAsync(new Guid());
 
             // Assert
             result.Should().BeNull();
@@ -129,15 +121,12 @@ namespace MinimalChessApi.Test.Unit
             var gameId = Guid.NewGuid();
             var filename = Path.Combine(TargetPath, $"{gameId}.game"); ;
 
-            var fileSystemMock = Substitute.For<IFileSystem>();
-            fileSystemMock.Directory.Exists(TargetPath).Returns(true);
-            fileSystemMock.File.Exists(filename).Returns(true);
-            fileSystemMock.File.ReadAllTextAsync(filename).Returns(async x => throw new Exception());
-
-            var sut = new FileStoreService(fileSystemMock, TargetPath);
+            _fileSystemMock.Directory.Exists(TargetPath).Returns(true);
+            _fileSystemMock.File.Exists(filename).Returns(true);
+            _fileSystemMock.File.ReadAllTextAsync(filename).Returns(async x => throw new Exception());
 
             // Act
-            var result = await sut.LoadGameAsync(new Guid());
+            var result = await _sut.LoadGameAsync(new Guid());
 
             // Assert
             result.Should().BeNull();
@@ -153,15 +142,12 @@ namespace MinimalChessApi.Test.Unit
                                new Guid("37b317a0-ff83-42e6-8175-54cf6e1a35e0"),
                                new Guid("ad90b4bc-27cb-455f-8439-bc2318ae656a")};
 
-            var fileSystemMock = Substitute.For<IFileSystem>();
-            fileSystemMock.Directory.Exists(TargetPath).Returns(true);
-            fileSystemMock.Directory.GetFiles(TargetPath, Arg.Any<string>())
+            _fileSystemMock.Directory.Exists(TargetPath).Returns(true);
+            _fileSystemMock.Directory.GetFiles(TargetPath, Arg.Any<string>())
                 .Returns(games.Select(g => Path.Combine(TargetPath, g+".game")).ToArray());
 
-            var sut = new FileStoreService(fileSystemMock, TargetPath);
-
             // Act
-            var result = await sut.GetGamesAsync();
+            var result = await _sut.GetGamesAsync();
 
             // Assert
             result.Should().BeEquivalentTo(games);
@@ -171,15 +157,12 @@ namespace MinimalChessApi.Test.Unit
         public async Task GetGamesAsync_NoGame_ReturnsEmptyList()
         {
             // Arrange
-            var fileSystemMock = Substitute.For<IFileSystem>();
-            fileSystemMock.Directory.Exists(TargetPath).Returns(true);
-            fileSystemMock.Directory.GetFiles(TargetPath, Arg.Any<string>())
+            _fileSystemMock.Directory.Exists(TargetPath).Returns(true);
+            _fileSystemMock.Directory.GetFiles(TargetPath, Arg.Any<string>())
                 .Returns(Array.Empty<string>().ToArray());
 
-            var sut = new FileStoreService(fileSystemMock, TargetPath);
-
             // Act
-            var result = await sut.GetGamesAsync();
+            var result = await _sut.GetGamesAsync();
 
             // Assert
             result.Should().BeEmpty();
